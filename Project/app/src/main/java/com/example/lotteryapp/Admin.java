e527b78ba9cce3758145f8daf42809ae745f97eb
Anshcool5 <anshul5@ualberta.ca>
2024-11-05 20:00:44 -0700

    Added Admin login and home page access

--- Project/app/src/main/java/com/example/lotteryapp/Admin.java
+++ Project/app/src/main/java/com/example/lotteryapp/Admin.java
@@ -1,28 +1,47 @@
 package com.example.lotteryapp;
 
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import android.content.Context;
+
 public class Admin extends User{
     //private EntrantsOrganizer entrantsOrganizer;
-    private Event eventsFacility;
+    //private Event eventsFacility;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private Context context;
 
     @Override
     public void displayUserInfo() {
 
     }
 
+    // Constructor
+    public Admin(Context context) {
+        this.context = context;
+    }
 
+    // Remove Event
+    public void removeEvent(String eventId) {
+        db.collection("events").document(eventId).delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(context, "Event deleted successfully", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(context, "Error deleting Event!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 /*
-        // Constructor
-    public Admin() {
-        //this.entrantsOrganizer = new EntrantsOrganizer();
-        this.eventsFacility = new EventsFacility();
-    }
-
-        // Remove methods
-        public void removeEvent(String eventQrHash) {
-        eventsFacility.deleteEvent(eventQrHash);
-    }
-
-        public void removeProfile(String profileId) {
+    public void removeProfile(String profileId) {
         entrantsOrganizer.deleteProfile(profileId);
     }
 

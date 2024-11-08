8dd31bd4cbebf0ef77ab9797010284486c871e0d
Anshcool5 <anshul5@ualberta.ca>
2024-11-08 01:57:27 -0700

    pulled from Main

--- Project/app/src/main/java/com/example/lotteryapp/AdminViewEditFacilitiesActivity.java
+++ Project/app/src/main/java/com/example/lotteryapp/AdminViewEditFacilitiesActivity.java
@@ -1,4 +1,19 @@
 package com.example.lotteryapp;
 
-public class AdminViewEditFacilitiesActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminViewEditFacilitiesActivity extends AppCompatActivity implements FacilityAdapter.FacilityClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_facilities_page);
+    }
+
+    @Override
+    public void onFacilityClick(Facility facility) {
+
+    }
 }

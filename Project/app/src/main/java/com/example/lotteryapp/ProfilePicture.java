db6d41673ed9855a9dffbb669df8b3e77f6e6efa
donna mathew <donnamathew280@gmail.com>
2024-11-10 16:31:11 -0700

    .

--- Project/app/src/main/java/com/example/lotteryapp/ProfilePicture.java
+++ Project/app/src/main/java/com/example/lotteryapp/ProfilePicture.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -29,24 +27,12 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.util.UUID;
-/**
- * The {@code ProfilePicture} class handles the user's profile picture selection and upload functionality.
- * It allows the user to pick an image from the device, display it, and upload it to Firebase Storage.
- * This class extends {@code AppCompatActivity}.
- *
- * @see AppCompatActivity
- * @see FirebaseStorage
- * @see ActivityResultLauncher
- * @version v1
- * @since v1
- * @author Team Aurora
- */
+
 public class ProfilePicture extends AppCompatActivity {
     StorageReference storageReference;
     LinearProgressIndicator progressIndicator;
     Uri image;
-    Button uploadImage;
-    ImageButton selectImage;
+    MaterialButton uploadImage, selectImage;
     ImageView imageView;
     private final ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
         @Override
@@ -63,23 +49,22 @@ public class ProfilePicture extends AppCompatActivity {
         }
     });
 
-    /**
-     * Initializes the activity, sets up Firebase Storage reference, and sets listeners for the select
-     * and upload image buttons.
-     *
-     * @param savedInstanceState the saved instance state of the activity
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.entrant_profile_edit);
+        setContentView(R.layout.profile_picture);
 
         FirebaseApp.initializeApp(ProfilePicture.this);
         storageReference = FirebaseStorage.getInstance().getReference();
 
-        imageView = findViewById(R.id.profile_photo);
-        selectImage = findViewById(R.id.add_profile_picture_button);
-        uploadImage = findViewById(R.id.confirm_changes);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        progressIndicator = findViewById(R.id.progress);
+
+        imageView = findViewById(R.id.imageView);
+        selectImage = findViewById(R.id.selectImage);
+        uploadImage = findViewById(R.id.uploadImage);
 
         selectImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -98,12 +83,6 @@ public class ProfilePicture extends AppCompatActivity {
         });
     }
 
-    /**
-     * Uploads the selected image to Firebase Storage and handles upload success, failure,
-     * and progress updates.
-     *
-     * @param file the {@code Uri} of the file to upload
-     */
     private void uploadImage(Uri file) {
         StorageReference ref = storageReference.child("images/" + UUID.randomUUID().toString());
         ref.putFile(file).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
